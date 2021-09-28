f8cc37cb3525022924ceab8898e3b43f097b4e23
Joshua-BerkleyKU <j708b537@ku.edu>
2021-09-27 22:21:25 -0500

    Update unparse.cpp

--- unparse.cpp
+++ unparse.cpp
@@ -308,7 +308,7 @@ void IfStmtNode::unparse(std::ostream& out, int indent) {
 	out << " If (";
 	this->condition->unparse(out, 0); 
 	out << ") {\n";
-	out << IfBody->size();
+	out << this->IfBody->size();
 	/*for (auto stmt: *IfBody)
 	{
 		stmt->unparse(out, indent);
